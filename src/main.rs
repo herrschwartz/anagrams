92e77ac16e6eec957cca7d775124bcf7e4a26077
GRANVILLEHOMES\tschwartz <tschwartz@assemigroup.com>
2019-12-23 15:19:09 -0800

    added the good algorithm

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use itertools::Itertools;
 use std::str;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::time::SystemTime;
 use rayon::prelude::*;
+use std::iter::FromIterator;
 
 fn main() {
+    //using http://wordlist.aspell.net/12dicts/
     let words = read_into_set("2of12.txt");
-    let now = SystemTime::now();
+    let words_dict = read_into_dict("words_encoded.txt");
+    let mut now = SystemTime::now();
     
-    let word = "sample";
+    let word = "bowling";
+    //The slow algorithm
     let anagrams = (3 ..= word.len())
         .into_par_iter()
         .map(|x| {
@@ -32,11 +36,35 @@ fn main() {
         println!("{:?}\n", m)
     });
     
-    //println!("{:?}", anagrams);
-    // let anagrams = get_words("fantastic", words);
-    //prettyprint_anagrams(anagrams);
-
     println!("{}", now.elapsed().unwrap().as_millis());
+    now = SystemTime::now();
+
+    //the better stronger faster algorithm
+    let anagrams_faster = (3 .. word.len())
+        .into_par_iter()
+        .map(|x| {
+            let mut anagrams = Vec::new();
+            for mut j in word.bytes().combinations(x) {
+                j.sort_unstable();
+                let wrd = words_dict.get(&String::from_utf8(j).unwrap());
+                if let Some(m) = wrd {
+                     anagrams.extend(m)
+                    //None => println!("failed to find"),
+                }
+            }
+            return anagrams;
+        });
+        let mut full: Vec<char> = word.chars().collect();
+        full.sort_unstable();
+
+        anagrams_faster.for_each(|mut m| {
+            m.sort_unstable();
+            m.dedup();
+
+            println!("{:?}\n", m)
+        });
+        println!("{:?}", words_dict.get(&String::from_iter(full)).unwrap_or(&[].to_vec()));
+        println!("{}", now.elapsed().unwrap().as_millis());
 }
 
 fn read_into_set(path: &str) -> HashSet<String>{
@@ -50,34 +78,29 @@ fn read_into_set(path: &str) -> HashSet<String>{
     return words;
 }
 
-fn get_words(word: &str, words: HashSet<String>, i: usize) -> Vec<String> {
-    let mut anagrams = Vec::new();
-    let mut cnt = 0;
-    // for i in 3..word.len()+1 {
-        for j in word.bytes().permutations(i) {
-            cnt += 1;
-            let wrd = str::from_utf8(&j).unwrap();
-            if words.contains(wrd){
-                anagrams.push(wrd.to_string())
-            }
-        }
-    // }
-    println!("{}",cnt);
-    return anagrams;
-}
+fn read_into_dict(path: &str) -> HashMap<String, Vec<String>> {
+    let mut words = HashMap::new();
+    let f = File::open(path).expect("failed opening");
+    let f = BufReader::new(f);
 
-fn prettyprint_anagrams(mut words: Vec<String>) {
-    words.sort_unstable();
-    words.dedup();
-    words.sort_unstable_by(|a, b| a.len().cmp(&b.len()));
-
-    let mut lastlen = 0;
-    for w in words {
-        if w.len() > lastlen{
-            println!("\n");
-        }
-        print!("{} ", w);
-        lastlen = w.len();
+    let mut wrds: Vec<String> = Vec::new();
+    let mut keys: Vec<String> = Vec::new();
+    for line in f.lines() {
+        let wr = line.unwrap();
+        let w: Vec<&str> = wr.split(';').collect();
+        keys.push(w[1].to_owned());
+        wrds.push(w[0].to_owned());
     }
-    //Wprintln!("\n {}", words.len())
+
+    for key in keys {
+        words.insert(key.to_string(), Vec::new());
+    }
+    for word in wrds {
+        let mut word_sorted: Vec<char> = word.chars().collect();
+        word_sorted.sort_unstable();
+        if let Some(c) = words.get_mut(&String::from_iter(word_sorted)){
+            c.push(word);
+        }
+    }
+    words
 }
